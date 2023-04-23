13191187dca380827a0b2112e6c13dde311e1f72
EseniyaKodenko <130905665+EseniyaKodenko@users.noreply.github.com>
2023-04-23 17:54:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,6 +6,6 @@ from hyppo.ksample import MMD
 chat_id = 1019285902 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    _, pvalue = MMD(compute_kernel='rbf', gamma = 0.1).test(x, y)
+    _, pvalue = MMD(compute_kernel='rbf', gamma = 10).test(x, y)
     ans = True if pvalue < 0.05 else False
     return ans # Ваш ответ, True или False
